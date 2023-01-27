b761209a7d57026cd83d30efd2e13a4769a04493
Joseph Sanchez <josephsanchez@josephs-mbp.local>
2023-01-27 00:03:01 -0800

    made the template and also added the service worker for production install

---
+++ client/src/App.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './utils/store';
+import Home from './pages/home';
+
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  {
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+          <div>
+            <Provider store={store}>
+              <Routes>
+              <Route
+                path='/'
+                element={<Home/>}
+              />
+              {
+              /*
+              <Route
+                path='/something'
+                element= {}
+              />
+              */
+              //This is all Routes
+              }
+              <Route
+                path='*'
+                element={<h1>Not Found</h1>}
+              />
+              </Routes>
+            </Provider>
+          </div>
+        </Router>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/navbar.js
@@ -0,0 +1,7 @@
+const Navbar = () => {
+    return (
+        <h1>Something</h1>
+    );
+};
+
+export default Navbar;
