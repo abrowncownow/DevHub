c9131e79d19587a60b1ab53444e3ea84cd8bb22d
LonelyMitoc <jamesmatsu@gmail.com>
2023-01-28 14:32:11 -0800

    user and project model update

--- server/models/Project.js
+++ server/models/Project.js
@@ -16,6 +16,17 @@ const projectSchema = new Schema({
   image: {
     type: String,
   },
+  projectCreator: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  discord: {
+    type: String,
+  },
+  goFundMe: {
+    type: String,
+  }
 });
 
 module.exports = projectSchema;
