ec02ef54b94e02c4de21abef0dcfcf5acdf15497
dankturtle <abrowncownow@gmail.com>
2023-01-30 21:44:30 -0800

    updated project, components, style

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Login from './components/login';
 import CreateProject from './pages/createProject';
 import Navbar from './components/navbar';
 import Signup from './components/signup';
+import SingleProject from './pages/singleProject';
 
 
 // Construct our main GraphQL API endpoint
@@ -63,6 +64,10 @@ function App() {
               <Route
                 path='/create'
                 element={<CreateProject />}
+              />
+              <Route
+                path = 'projects/:projectId'
+                element = {<SingleProject />}
               />
               <Route
                 path='*'
