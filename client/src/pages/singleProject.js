613fa45e71ce8cf98593472b137c0feff490c49b
dankturtle <abrowncownow@gmail.com>
2023-02-01 22:00:07 -0800

    added button

--- client/src/pages/singleProject.js
+++ client/src/pages/singleProject.js
@@ -3,26 +3,38 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import ProjectEdit from "../components/projectEdit";
 import ProjectView from "../components/projectView";
-
+import { QUERY_ME } from "../utils/queries";
 import { QUERY_SINGLE_PROJECT } from '../utils/queries';
-
-
+import Auth from "../utils/auth";
+const SignedIn = Auth.loggedIn() ? true : false;
 
 //data prop
 
 const SingleProject = () => {
-
+  let [authEditor, toggleAuthEditor] = useState(false);
   const [editMode, toggleEditMode] = useState(false);
   const { projectId } = useParams();
   const { loading, data } = useQuery(QUERY_SINGLE_PROJECT, {
     variables: {projectId: projectId},
   });
-  
   let project;
   if (data ? true : false){
-  project = data?.project || {};
-  console.log(project);
-}
+    project = data?.project || {};
+  }
+ 
+  let queryMe = useQuery(QUERY_ME);
+  if ((data && queryMe) ? true : false){
+    checkUsers()
+  }
+  async function checkUsers(){
+    let currentUser = queryMe?.data || {};
+      currentUser = currentUser.user;
+    if (project.projectCreator === currentUser._id){
+      authEditor = true;
+      console.log(authEditor)
+    }
+    }
+
 
 return(
   <div>
@@ -32,12 +44,13 @@ return(
       </div>
     ): (
       <div>
-      {editMode ? (
-        <ProjectEdit project={project} projectId = {projectId} />
-      ):(
-        <ProjectView project={project} projectId = {projectId} />
+      {editMode ? (<ProjectEdit project={project} projectId = {projectId} />)
+      :(<ProjectView project={project} projectId = {projectId} />)}
+      </div>
       )}
-      </div>)}
+            {authEditor ?(
+        <button>Edit Page</button>):(
+        <div></div>)}
   </div>
     );
 };
