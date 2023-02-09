4ea26ec29c19adce82c72e743a4408e7e0fa331e
Joseph <joeychez23@outlook.com>
2023-02-08 17:53:25 -0800

    remove console.log

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -13,7 +13,7 @@ const Home = () => {
 
   if (data ? true : false) {
     projectData = data?.projects || {};
-    console.log(projectData)
+    //console.log(projectData)
   }
 
   useEffect(() => { }, [])
